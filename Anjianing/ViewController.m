e24ec9baeaaa8720854f0d599fea22400deba3a6
liyusheng <liyusheng@xuetangx.com>
2017-06-29 11:11:38 +0800

    init project

--- Anjianing/ViewController.m
+++ Anjianing/ViewController.m
@@ -9,14 +9,26 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIWebView *webview;
 
 @end
 
 @implementation ViewController
-
+/**
+ - webView:shouldStartLoadWithRequest:navigationType:
+ Sent before a web view begins loading a frame.
+ - webViewDidStartLoad:
+ Sent after a web view starts loading a frame.
+ - webViewDidFinishLoad:
+ Sent after a web view finishes loading a frame.
+ - webView:didFailLoadWithError:
+ Sent if a web view failed to load a frame.
+ 
+ http://anjiani.weyangyang.com/xmshop/index?uid=2
+ */
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self.webview loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://anjiani.weyangyang.com/xmshop/index?uid=2"]]];
 }
 
 
